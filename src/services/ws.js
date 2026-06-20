bd78fef9cc498acf280001d986ac60ab7f3f81a1
Dinesh <124895635+Dinesh1441@users.noreply.github.com>
2026-06-20 15:22:27 +0530

    fix origin

--- src/services/ws.js
+++ src/services/ws.js
@@ -5,7 +5,7 @@ let io
 function initializeSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173', // Replace with your frontend URL
+      origin: 'https://chat-7myy.vercel.app/', // Replace with your frontend URL
       credentials: true,
       methods: ['GET', 'POST'],
     },
